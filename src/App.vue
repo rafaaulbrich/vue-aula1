5ce868baed90cc051e3587f185fb40481c51763d
rafaaulbrich <rafaelacamilly2018@gmail.com>
2023-02-27 16:14:32 -0300

    commit-3

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+
+const contador = ref(0)
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  contador.value--
+}
+
+onMounted(() => {
+  console.log(`O valor inicial do contador é ${contador.value}.`)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <p>Valor do contador é: {{ contador }}</p>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  font-weight: bold;
 }
 </style>
